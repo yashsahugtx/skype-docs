7c8ae7b6358b25f51aae06da623fd364106ac9b9
Sandeep Kumar (SKYPE) <sankum@microsoft.com>
2017-03-28 16:06:45 -0700

    PlatformSamples: Update InviteAndRemoveParticipant to use WebEventChannel

--- Skype/Trusted-Application-API/samples/QuickStartSamples/InviteAndRemoveParticipant/Program.cs
+++ Skype/Trusted-Application-API/samples/QuickStartSamples/InviteAndRemoveParticipant/Program.cs
@@ -4,9 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.SfB.PlatformService.SDK.ClientModel;
 using Microsoft.SfB.PlatformService.SDK.ClientModel.Internal; // Required for setting customized callback url
 using Microsoft.SfB.PlatformService.SDK.Common;
-using Microsoft.Skype.Calling.ServiceAgents.SkypeToken;
 using QuickSamplesCommon;
-using TrouterCommon;
 
 namespace InviteAndRemoveParticipant
 {
@@ -17,16 +15,16 @@ namespace InviteAndRemoveParticipant
             var sample = new InviteAndRemoveParticipant();
             try
             {
-                sample.RunAsync().Wait();
+                Uri callbackUri;
+                // Start HTTP server and get callback uri
+                using (WebEventChannel.WebEventChannel.StartHttpServer(out callbackUri))
+                {
+                    sample.RunAsync(callbackUri).Wait();
+                }
             }
             catch (AggregateException ex)
             {
                 Console.WriteLine("Exception: " + ex.GetBaseException().ToString());
-            }
-
-            if(sample.EventChannel != null)
-            {
-                sample.EventChannel.TryStopAsync().Wait();
             }
         }
     }
@@ -40,30 +38,20 @@ namespace InviteAndRemoveParticipant
     /// </summary>
     internal class InviteAndRemoveParticipant
     {
-        public TrouterBasedEventChannel EventChannel { get; private set; }
-
         private IPlatformServiceLogger m_logger;
+        private TaskCompletionSource<IParticipant> m_participantAddedTcs;
 
-        public async Task RunAsync()
+        public async Task RunAsync(Uri callbackUri)
         {
-            var skypeId = ConfigurationManager.AppSettings["Trouter_SkypeId"];
-            var password = ConfigurationManager.AppSettings["Trouter_Password"];
-            var applicationName = ConfigurationManager.AppSettings["Trouter_ApplicationName"];
-            var userAgent = ConfigurationManager.AppSettings["Trouter_UserAgent"];
             var participantUri = ConfigurationManager.AppSettings["ParticipantUri"];
-            var token = SkypeTokenClient.ConstructSkypeToken(
-                skypeId: skypeId,
-                password: password,
-                useTestEnvironment: false,
-                scope: string.Empty,
-                applicationName: applicationName).Result;
 
             m_logger = new SampleAppLogger();
 
             // Uncomment for debugging
             // m_logger.HttpRequestResponseNeedsToBeLogged = true;
 
-            EventChannel = new TrouterBasedEventChannel(m_logger, token, userAgent);
+            // You can hook up your own implementation of IEventChannel here
+            IEventChannel eventChannel = WebEventChannel.WebEventChannel.Instance;
 
             // Prepare platform
             var platformSettings = new ClientPlatformSettings(QuickSamplesConfig.AAD_ClientSecret, new Guid(QuickSamplesConfig.AAD_ClientId));
@@ -71,7 +59,7 @@ namespace InviteAndRemoveParticipant
 
             // Prepare endpoint
             var endpointSettings = new ApplicationEndpointSettings(new SipUri(QuickSamplesConfig.ApplicationEndpointId));
-            var applicationEndpoint = new ApplicationEndpoint(platform, endpointSettings, EventChannel);
+            var applicationEndpoint = new ApplicationEndpoint(platform, endpointSettings, eventChannel);
 
             var loggingContext = new LoggingContext(Guid.NewGuid());
             await applicationEndpoint.InitializeAsync(loggingContext).ConfigureAwait(false);
@@ -87,13 +75,15 @@ namespace InviteAndRemoveParticipant
             WriteToConsoleInColor("ad hoc meeting join url : " + adhocMeeting.JoinUrl);
 
             // Get all the events related to join meeting through Trouter's uri
-            platformSettings.SetCustomizedCallbackurl(new Uri(EventChannel.CallbackUri));
+            platformSettings.SetCustomizedCallbackurl(callbackUri);
 
             // Start joining the meeting
             var invitation = await adhocMeeting.JoinAdhocMeeting(loggingContext, null).ConfigureAwait(false);
 
             // Wait for the join to complete
             await invitation.WaitForInviteCompleteAsync().ConfigureAwait(false);
+
+            m_participantAddedTcs = new TaskCompletionSource<IParticipant>();
 
             invitation.RelatedConversation.HandleParticipantChange += Conversation_HandleParticipantChange;
 
@@ -104,14 +94,15 @@ namespace InviteAndRemoveParticipant
             // Wait for the join to complete
             await participantInvitation.WaitForInviteCompleteAsync().ConfigureAwait(false);
 
-            //remove the participant from the meeting
-            foreach (var participant in invitation.RelatedConversation.Participants)
-            {
-                if (participantUri.Equals(participant.Uri))
-                {
-                    await participant.EjectAsync(loggingContext).ConfigureAwait(false);
-                }
-            }
+            // Get the corresponding IParticipant; other way of getting this is by traversing invitation.RelatedConversation.Participants
+            IParticipant participant = await m_participantAddedTcs.Task.ConfigureAwait(false);
+
+            WriteToConsoleInColor("Waiting 10 seconds before ejecting " + participant.Name);
+            await Task.Delay(TimeSpan.FromSeconds(10)).ConfigureAwait(false);
+
+            WriteToConsoleInColor("Ejecting " + participant.Name + " from the meeting");
+
+            await participant.EjectAsync(loggingContext).ConfigureAwait(false);
 
             WriteToConsoleInColor("Showing roaster udpates for 5 minutes for meeting : " + adhocMeeting.JoinUrl);
 
@@ -119,6 +110,8 @@ namespace InviteAndRemoveParticipant
             // Since we have registered Conversation_HandleParticipantChange, we will continue to show participant changes in the
             // meeting for this duration.
             await Task.Delay(TimeSpan.FromMinutes(5)).ConfigureAwait(false);
+
+            await WebEventChannel.WebEventChannel.Instance.TryStopAsync().ConfigureAwait(false);
         }
 
         private void Conversation_HandleParticipantChange(object sender, ParticipantChangeEventArgs eventArgs)
@@ -128,6 +121,7 @@ namespace InviteAndRemoveParticipant
                 foreach (var participant in eventArgs.AddedParticipants)
                 {
                     WriteToConsoleInColor(participant.Name + " has joined the meeting.");
+                    m_participantAddedTcs.SetResult(participant);
                 }
             }
 

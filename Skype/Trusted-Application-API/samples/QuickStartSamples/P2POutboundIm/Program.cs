9f453eb2a8ecd89d4288f9eecf345bc4b3830205
Sandeep Kumar (SKYPE) <sankum@microsoft.com>
2017-03-29 15:23:03 -0700

    PlatformSamples: Update P2PImOutboundCall to use WebEventChannel

--- Skype/Trusted-Application-API/samples/QuickStartSamples/P2POutboundIm/Program.cs
+++ Skype/Trusted-Application-API/samples/QuickStartSamples/P2POutboundIm/Program.cs
@@ -4,9 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.SfB.PlatformService.SDK.ClientModel;
 using Microsoft.SfB.PlatformService.SDK.Common;
-using Microsoft.Skype.Calling.ServiceAgents.SkypeToken;
 using QuickSamplesCommon;
-using TrouterCommon;
 
 namespace P2POutboundIm
 {
@@ -17,43 +15,35 @@ namespace P2POutboundIm
             var sample = new P2PImOutboundCall();
             try
             {
-                sample.RunAsync().Wait();
+                Uri callbackUri;
+                // Start HTTP server and get callback uri
+                using (WebEventChannel.WebEventChannel.StartHttpServer(out callbackUri))
+                {
+                    sample.RunAsync(callbackUri).Wait();
+                }
             }
             catch (ArgumentException ex)
             {
                 Console.WriteLine("Exception: " + ex.GetBaseException());
             }
-
-            sample.EventChannel?.TryStopAsync().Wait();
         }
     }
 
     internal class P2PImOutboundCall
     {
-        public TrouterBasedEventChannel EventChannel { get; private set; }
-
         private IPlatformServiceLogger m_logger;
 
-        public async Task RunAsync()
+        public async Task RunAsync(Uri callbackUri)
         {
-            var skypeId = ConfigurationManager.AppSettings["Trouter_SkypeId"];
-            var password = ConfigurationManager.AppSettings["Trouter_Password"];
-            var applicationName = ConfigurationManager.AppSettings["Trouter_ApplicationName"];
-            var userAgent = ConfigurationManager.AppSettings["Trouter_UserAgent"];
             var targetUserId = ConfigurationManager.AppSettings["Skype_TargetUserId"];
-            var token = SkypeTokenClient.ConstructSkypeToken(
-                skypeId: skypeId,
-                password: password,
-                useTestEnvironment: false,
-                scope: string.Empty,
-                applicationName: applicationName).Result;
 
             m_logger = new SampleAppLogger();
 
             // Uncomment for debugging
             // m_logger.HttpRequestResponseNeedsToBeLogged = true;
 
-            EventChannel = new TrouterBasedEventChannel(m_logger, token, userAgent);
+            // You can hook up your own implementation of IEventChannel here
+            IEventChannel eventChannel = WebEventChannel.WebEventChannel.Instance;
 
             // Prepare platform
             var platformSettings = new ClientPlatformSettings(QuickSamplesConfig.AAD_ClientSecret,
@@ -62,7 +52,7 @@ namespace P2POutboundIm
 
             // Prepare endpoint
             var endpointSettings = new ApplicationEndpointSettings(new SipUri(QuickSamplesConfig.ApplicationEndpointId));
-            var applicationEndpoint = new ApplicationEndpoint(platform, endpointSettings, EventChannel);
+            var applicationEndpoint = new ApplicationEndpoint(platform, endpointSettings, eventChannel);
 
             var loggingContext = new LoggingContext(Guid.NewGuid());
             await applicationEndpoint.InitializeAsync(loggingContext).ConfigureAwait(false);
@@ -72,18 +62,25 @@ namespace P2POutboundIm
             var invitation = await applicationEndpoint.Application.Communication.StartMessagingAsync(
                 subject: "Subject",
                 to: targetUserId,
-                callbackUrl: EventChannel.CallbackUri,
-                loggingContext: loggingContext);
+                callbackUrl: callbackUri.ToString(),
+                loggingContext: loggingContext).ConfigureAwait(false);
+
+            // Wait for user to accept the invitation
             var conversation = await invitation.WaitForInviteCompleteAsync().ConfigureAwait(false);
 
             conversation.HandleParticipantChange += Conversation_HandleParticipantChange;
             conversation.MessagingCall.IncomingMessageReceived += Handle_IncomingMessage;
 
-            WriteToConsoleInColor("Showing roaster udpates for 5 minutes for the messages");
+            // Send the initial message
+            await conversation.MessagingCall.SendMessageAsync("Hello World!", loggingContext).ConfigureAwait(false);
+
+            WriteToConsoleInColor("Staying in the conversation for 5 minutes");
             
             // Wait 5 minutes before exiting
             // Since we registered callbacks, we will continue to show message logs
             await Task.Delay(TimeSpan.FromMinutes(5)).ConfigureAwait(false);
+
+            await WebEventChannel.WebEventChannel.Instance.TryStopAsync().ConfigureAwait(false);
         }
 
         private void Handle_IncomingMessage(object sender, IncomingMessageEventArgs incomingMessageEventArgs)
